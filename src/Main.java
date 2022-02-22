4c1cfe3983c1908bf02b5dd32aa18ec266b10cce
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-02-22 12:07:05 -0300

    Atualização de parâmetros

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Jardel");
+		Cliente jardel = new Cliente();
+		jardel.setNome("Jardel");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(jardel);
+		Conta poupanca = new ContaPoupanca(jardel);
 
 		cc.depositar(1000);
 		cc.transferir(500, poupanca);
